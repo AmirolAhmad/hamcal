aa1eefb49fe67c58cf4f683aab969d2b48518287
AmirolAhmad <9w2pdf@gmail.com>
2017-05-24 01:02:48 +0800

    added category model & seed file

---
+++ app/models/category.rb
@@ -0,0 +1,8 @@
+class Category < ApplicationRecord
+  extend FriendlyId
+  friendly_id :name, use: [:slugged, :finders, :history]
+
+  def should_generate_new_friendly_id?
+    name_changed?
+  end
+end
